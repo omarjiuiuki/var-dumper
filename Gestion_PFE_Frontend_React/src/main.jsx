3f3d7018bf2dab3b5cc2cb465f17425da375b8e9
BekhechiWahib <wahibekhechi@gmail.com>
2024-11-28 15:12:48 +0100

    commit-main

--- Gestion_PFE_Frontend_React/src/main.jsx
+++ Gestion_PFE_Frontend_React/src/main.jsx
@@ -2,10 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import DashboardAdminMain from './dashboard_admin_main.jsx'
-import DashboardEnseignantMain from './views/dashboard enseignant/dashboard_enseignant_main.jsx'
-import DashboardEnterpriseMain from './views/dashboard entreprise/dashboard_entreprise_main.jsx'
-import DashboardEtudiantMain from './views/dashboard etudiant/dashboard_etudiant_main.jsx'
+import DashboardAdminMain from './views/dashboard admin/pages/dashboard_admin_main.jsx'
 
 
 createRoot(document.getElementById('root')).render(
